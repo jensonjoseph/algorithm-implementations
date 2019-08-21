216b04b8526ed3392c757aeb76efa2f823a03b53
Kakkattil, Jenson Jos <jenson.kakkattil@ge.com>
2019-08-21 13:52:07 -0400

    first commit

---
+++ src/main/java/com/jensonjo/algorithmimplementations/AlgorithmImplementationsApplication.java
@@ -0,0 +1,13 @@
+package com.jensonjo.algorithmimplementations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmImplementationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmImplementationsApplication.class, args);
+	}
+
+}
