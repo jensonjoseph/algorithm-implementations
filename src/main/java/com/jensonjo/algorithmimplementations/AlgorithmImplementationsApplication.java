ea9e9471db4a00a972619b2ef9cc8811f9c3da67
Kakkattil, Jenson Jos <jenson.kakkattil@ge.com>
2019-08-22 10:48:31 -0400

    enabling cli

--- src/main/java/com/jensonjo/algorithmimplementations/AlgorithmImplementationsApplication.java
+++ src/main/java/com/jensonjo/algorithmimplementations/AlgorithmImplementationsApplication.java
@@ -1,13 +1,19 @@
 package com.jensonjo.algorithmimplementations;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgorithmImplementationsApplication {
+public class AlgorithmImplementationsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlgorithmImplementationsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmImplementationsApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Welcome welcome = new Welcome();
+        welcome.greet();
+    }
 }
---
+++ src/main/java/com/jensonjo/algorithmimplementations/Welcome.java
@@ -0,0 +1,10 @@
+package com.jensonjo.algorithmimplementations;
+
+/**
+ * Created by jensonkakkattil on Aug, 2019.
+ */
+public class Welcome {
+    public void greet() {
+        System.out.print("Hello");
+    }
+}
